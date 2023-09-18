a88fb534cae5608e05e79fb98e27dc6f0fd52aea
Daniel Hoppe Hansen <danielhoppe123@gmail.com>
2023-09-18 12:43:03 +0200

    Tilføjede et tomt program til at begynde at kode i.

---
+++ Budwegkode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Budwegkode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
