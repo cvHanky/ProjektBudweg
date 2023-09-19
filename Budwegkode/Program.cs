31d96b6a07e5e540dad49920561a633b9fe45a79
Daniel Hoppe Hansen <danielhoppe123@gmail.com>
2023-09-19 12:57:45 +0200

    Rykkede rundt på noget tekst.

--- Budwegkode/Program.cs
+++ Budwegkode/Program.cs
@@ -6,6 +6,8 @@
         {
             // Nu opstilles en simpel menu som kan bruges i projektet.
 
+            Console.WriteLine("Velkommen til menuen.");
+
             List<string> UserList = new List<string>();
             UserList.Add("cvhanky");UserList.Add("simsimon");UserList.Add("morten"); UserList.Add("hankyboy");
 
@@ -13,7 +15,6 @@
             int countOuter = 0;
             while (menuRunning)
             {
-                Console.WriteLine("Velkommen til menuen.");
                 Console.WriteLine("\nIndtast medarbejder ID...");
                 string userID = Console.ReadLine();
                 bool user = false;     // En bool der styrer om brugeren er en valid user.
@@ -38,7 +39,7 @@
                     Console.WriteLine("Login failed."); // Testing123
                 }
             }
-            Console.Write("Press any button to continue...");
+            Console.Write("\nPress any button to continue...");
             Console.ReadKey();
         }
     }
