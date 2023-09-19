a60ec94598882c11d6bffa94f30e796997ba10a0
Daniel Hoppe Hansen <danielhoppe123@gmail.com>
2023-09-19 11:58:30 +0200

    Tilføjede en simpel login-applikation.

--- Budwegkode/Program.cs
+++ Budwegkode/Program.cs
@@ -4,9 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! Jeg hedder Daniel");
+            // Nu opstilles en simpel menu som kan bruges i projektet.
 
-            Console.WriteLine("Ny ændring 123123");
+            List<string> UserList = new List<string>();
+            UserList.Add("cvhanky");UserList.Add("simsimon");UserList.Add("morten"); UserList.Add("hankyboy");
+
+            bool menuRunning = true;     // En bool der styrer om menuen kører.
+            int countOuter = 0;
+            while (menuRunning)
+            {
+                Console.WriteLine("Velkommen til menuen.");
+                Console.WriteLine("\nIndtast medarbejder ID...");
+                string userID = Console.ReadLine();
+                bool user = false;     // En bool der styrer om brugeren er en valid user.
+                for (int i = 0; i < UserList.ToArray().Length; i++)
+                {              // Dette loop tjekker om brugeren er en valid user.
+                    if (userID == UserList[i])
+                    {
+                        user = true;
+                        Console.Clear();
+                        break;
+                    }
+                    else
+                        Console.Clear();
+                }
+                if (user)
+                {
+                    Console.WriteLine("Login successful.");
+                }
+                else
+                {
+                    Console.WriteLine("Login failed.");
+                }
+            }
 
             Console.ReadKey();
         }
