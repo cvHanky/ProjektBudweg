8cdf0d518fb340760cc3f7697d2cf471f360ebf8
Mads Juul Carlsen <mjca59231@edu.ucl.dk>
2023-09-21 13:46:34 +0200

    Tilføjede Afdeling-klasse til projektet.

---
+++ Budwegkode/Afdeling.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budwegkode
+{
+    public class Afdeling
+    {
+        // Klasse til Afdeling. 
+        public string Name;
+        public string Building;
+
+        public Afdeling(string name, string building)
+        {
+            Name = name;
+            Building = building;
+        }
+
+
+    }
+}
