df0ceb56bd2d0b7b99f2cfa94b8e750818fbd088
Mads Juul Carlsen <mjca59231@edu.ucl.dk>
2023-09-21 14:00:57 +0200

    Tilføjede klasse til besøgende grupper.

---
+++ Budwegkode/Besøgende.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budwegkode
+{
+    public class Besøgende
+    {
+        // Klasse til grupper der besøger virksomheden.
+        public int Antal;
+        public string Navn;
+
+        public Besøgende(int antal, string navn)
+        {
+            Antal = antal;
+            Navn = navn;
+        }
+    }
+}
